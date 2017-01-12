9587d5d13d10dbf99738bcae52e1312d086ff171
firestorm942 <speed9425@gmail.com>
2017-01-12 00:30:13 -0600

    update

--- src/net/lostplay/upnp/main.java
+++ src/net/lostplay/upnp/main.java
@@ -1,24 +1,11 @@
 package net.lostplay.upnp;
 
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * Created by firestorm942 on 1/11/2017.
- */
+
 public class main extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("hello World");
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("reload")) {
-            sender.sendMessage(ChatColor.GRAY + "reload");
-        }
-        return false;
     }
 }
