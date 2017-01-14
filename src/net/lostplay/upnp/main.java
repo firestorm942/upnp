fadda6f52142ac9c3a3dd40d1153316dfa08899f
firestorm942 <speed9425@gmail.com>
2017-01-14 13:55:15 -0600

    latest

--- src/net/lostplay/upnp/main.java
+++ src/net/lostplay/upnp/main.java
@@ -2,96 +2,60 @@ package net.lostplay.upnp;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.fourthline.cling.UpnpService;
-import org.fourthline.cling.UpnpServiceImpl;
-import org.fourthline.cling.registry.RegistryListener;
-import org.fourthline.cling.support.igd.PortMappingListener;
 import org.fourthline.cling.support.model.PortMapping;
 
 import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.Enumeration;
-import java.util.logging.Logger;
+import java.net.UnknownHostException;
 public class main
         extends JavaPlugin
 {
-    private Logger log = Logger.getLogger("Minecraft");
-    private boolean failed = false;
-    private UpnpService upnp;
-
+    private static UpnpService upnpService = null;
     public void onEnable()
     {
-        this.log.info("[AutoUpnp] AutoUpnp v1.0 by firestorm942 starting!");
-        this.log.info("[AutoUpnp] Attempting to forward port: " + getServer().getPort());
-
-        forwardPort(getServer().getPort());
-    }
-
-    private void forwardPort(int port)
-    {
-        String address = null;
-        try
-        {
-            address = getInternalIP();
-        }
-        catch (SocketException e)
-        {
-            this.log.info("[AutoUpnp] Failed to forward port. :-( Stack trace: ");
+        getLogger();
+        String protocol;
+        String name;
+        String ip;
+        int port;
+        getConfig();
+        getConfig().addDefault("settings.protocol", String.valueOf("TCP"));
+        getConfig().addDefault("settings.name", String.valueOf("minecraft"));
+        try {
+            getConfig().addDefault("settings.ipaddress", InetAddress.getLocalHost().getHostAddress());
+        } catch (UnknownHostException e) {
             e.printStackTrace();
-            this.failed = true;
-            disable();
         }
-        PortMapping pm = new PortMapping(port, address, PortMapping.Protocol.TCP, "Minecraft");
-        this.upnp = new UpnpServiceImpl(new RegistryListener[] { new PortMappingListener(pm)
-        {
-            public void handleFailureMessage(String s)
-            {
-                main.this.log.severe("[AutoUpnp] Failed to forward port. :-( Error message: " + s);
-                main.this.failed = true;
-                main.this.disable();
-            }
-        } });
-        this.upnp.getControlPoint().search();
+        getConfig().addDefault("settings.port", Integer.valueOf(25565));
+        getConfig().options().copyDefaults(true);
+        protocol = getConfig().getString("settings.protocol");
+        name = getConfig().getString("settings.name");
+        ip = getConfig().getString("settings.ipaddress");
+        port = getConfig().getInt("settings.port");
+        saveConfig();
+        getLogger().info("[AutoUpnp] AutoUpnp v1.0 by firestorm942 starting!");
+        getLogger().info("[AutoUpnp] Attempting to forward port: " + getServer().getPort());
+        openPort(ip, port, name, protocol);
     }
 
-    private String getInternalIP()
-            throws SocketException
+    private static void openPort(String ip, int port, String name, String protocol)
     {
-        Enumeration<NetworkInterface> nics = NetworkInterface.getNetworkInterfaces();
-        while (nics.hasMoreElements())
-        {
-            NetworkInterface nic = (NetworkInterface)nics.nextElement();
-            if (!nic.isLoopback()) {
-                return ((InetAddress)nic.getInetAddresses().nextElement()).getHostAddress();
-            }
+        PortMapping mapping;
+        UpnpService upnpService;
+        if (protocol == "TCP") {
+            mapping = new PortMapping(port, ip, PortMapping.Protocol.TCP, name);
+        } else if (protocol == "UDP") {
+            mapping = new PortMapping(port, ip, PortMapping.Protocol.UDP, name);
+        } else {
+            mapping = new PortMapping(port, ip, PortMapping.Protocol.TCP, name);
         }
-        return null;
+        upnpService = new org.fourthline.cling.UpnpServiceImpl(new org.fourthline.cling.registry.RegistryListener[] { new org.fourthline.cling.support.igd.PortMappingListener(mapping) });
+        upnpService.getControlPoint().search();
     }
 
-    private void disable()
-    {
-        getServer().getPluginManager().disablePlugin(this);
-    }
 
     public void onDisable()
     {
-        this.log.info("[AutoUpnp] Disabling..");
-        if (!this.failed)
-        {
-            String address = null;
-            try
-            {
-                address = getInternalIP();
-            }
-            catch (SocketException e)
-            {
-                this.log.info("[AutoUpnp] Failed to remove port mapping. :-( Stack trace: ");
-                e.printStackTrace();
-                this.failed = true;
-                disable();
-            }
-            this.log.info("[AutoUpnp] Removing port mapping");
-            this.upnp.shutdown();
-        }
+        getLogger().info("[AutoUpnp] Removing port mapping.");
+        upnpService.shutdown();
     }
 }
